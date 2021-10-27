64d59227a28ee8ab7e0ed9e1f6af7d2187709971
Santi <santima1904@gmail.com>
2021-10-27 10:11:55 +0200

    File object hecha, falta borrar y consultar

--- Practica_1T_ACDAT/src/modelo/ficheros/FileAccessObject.java
+++ Practica_1T_ACDAT/src/modelo/ficheros/FileAccessObject.java
@@ -1,20 +1,16 @@
 package modelo.ficheros;
 
 import modelo.Cliente;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+
+import java.io.*;
 
 public class FileAccessObject {
 
     //Constantes
     public static final String FICHERO_CLIENTES = "clientes.bin";
-    public static final String FICHERO_INDICE = "indice_clientes.bin";
 
     //Propiedades estáticas
     private static File ficheroClientes = new File(FICHERO_CLIENTES);
-    private static File ficheroIndice = new File(FICHERO_INDICE);
 
 
     /**
@@ -52,5 +48,58 @@ public class FileAccessObject {
         }
     }
 
+    /**
+     * <h1>Cabecera: </h1>public static int buscarPosicion(String dni)<br/>
+     * <h1>Descripción: </h1> Método para buscar la posición del cliente con el dni dado <br/>
+     * <h1>Precondiciones: </h1>Fichero creado y dni diferente de null <br/>
+     * <h1>Postocondiciones: </h1>Posicion encontrada<br/>
+     * <br/>
+     * <h1>Entradas: </h1>String dni <br/>
+     * <h1>Salidas: </h1>int
+     *
+     * @param dni
+     */
+    public static int buscarPosicion(String dni){
+        int contador = 0;
+        boolean valido = false;
+        Object aux = new Object();
 
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ficheroClientes))){
+            while (aux != null || !valido) {
+                aux = ois.readObject();
+                contador++;
+                valido = comprobarDni((Cliente) aux, dni);
+            }
+            if (!valido){
+                contador = -1;
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return contador;
+    }
+
+    /**
+     * <h1>Cabecera: </h1>private static boolean comprobarDni(Cliente cliente, String dni)<br/>
+     * <h1>Descripción: </h1> Método para comprobar que el dni dado y el del cliente coinciden<br/>
+     * <br/>
+     * <h1>Entradas: </h1>Cliente cliente, String dni<br/>
+     * <h1>Salidas: </h1>boolean
+     *
+     * @param dni
+     * @param cliente
+     */
+    private static boolean comprobarDni(Cliente cliente, String dni){
+        boolean igual = false;
+
+        if (cliente.getDni().equals(dni)){
+            igual = true;
+        }
+        return igual;
+    }
 }
--- Practica_1T_ACDAT/src/modelo/ficheros/FileAccessText.java
+++ Practica_1T_ACDAT/src/modelo/ficheros/FileAccessText.java
@@ -8,6 +8,13 @@ public class FileAccessText{
 
     //Constantes
     public static final String FICHERO_PRINCIPAL = "configuracion.bin";
+    public static final String FICHERO_INDICE = "indice_clientes.bin";
+    public static final String FICHERO_TEXT = "listado_clientes.bin";
+
+    //Propiedades estáticas
+    private static File ficheroConfiguracion = new File(FICHERO_PRINCIPAL);
+    private static File ficheroText = new File(FICHERO_TEXT);
+    private static File ficheroIndice = new File(FICHERO_INDICE);
 
     /**
      * <h1>Cabecera: </h1>public File inicializarFichero()<br/>
@@ -15,16 +22,14 @@ public class FileAccessText{
      *
      * @return ficheroConfiguracion
      */
-    public static File inicializarFichero() {
-        File ficheroconfiguracion = new File(FICHERO_PRINCIPAL, ".");
+    public static void inicializarFichero() {
 
         try {
-            ficheroconfiguracion.createNewFile();
-            escribirCodificacionFichero("UTF-8", ficheroconfiguracion);
+            ficheroConfiguracion.createNewFile();
+            escribirCodificacionFichero("UTF-8");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return ficheroconfiguracion;
     }
 
     /**
@@ -36,12 +41,11 @@ public class FileAccessText{
      * <h1>Entradas: </h1>String codificacion, File fichero <br/>
      * <h1>Salidas: </h1>Ninguna
      *
-     * @param fichero
      * @param codificacion
      */
-    public static void escribirCodificacionFichero(String codificacion, File fichero) {
+    public static void escribirCodificacionFichero(String codificacion) {
 
-        try(FileWriter fw = new FileWriter(fichero, false)) {
+        try(FileWriter fw = new FileWriter(ficheroConfiguracion, false)) {
             fw.write(codificacion);
         } catch (IOException e) {
             e.printStackTrace();
@@ -57,13 +61,12 @@ public class FileAccessText{
      * <h1>Entradas: </h1>List<Cliente> lista, File fichero, String formato <br/>
      * <h1>Salidas: </h1>Ninguna
      *
-     * @param fichero
      * @param lista
      * @param formato
      */
-    public static void escribirClientesFichero(List<Cliente> lista, File fichero, String formato) {
+    public static void escribirClientesFichero(List<Cliente> lista, String formato) {
 
-        try(OutputStreamWriter osw = new OutputStreamWriter((new FileOutputStream(fichero, true)), leerFicheroText(fichero))) {
+        try(OutputStreamWriter osw = new OutputStreamWriter((new FileOutputStream(ficheroText, true)), leerFicheroText(ficheroConfiguracion))) {
             for (Cliente cliente:lista){
                 osw.write(cliente.toString());
                 osw.write(System.lineSeparator());
@@ -96,5 +99,30 @@ public class FileAccessText{
             e.printStackTrace();
         }
         return formato;
+    }
+
+    /**
+     * <h1>Cabecera: </h1>private static String leerFicheroText(File fichero)<br/>
+     * <h1>Descripción: </h1> Método para leer el fichero de texto <br/>
+     * <h1>Precondiciones: </h1>Fichero creado<br/>
+     * <h1>Postocondiciones: </h1>Formato leido<br/>
+     * <br/>
+     * <h1>Entradas: </h1>File fichero<br/>
+     * <h1>Salidas: </h1>String formato
+     *
+     * @return formato
+     */
+    private static void escribirFicheroIndice(String dni, int posicion){
+
+        try(DataOutputStream dos = new DataOutputStream(new FileOutputStream(ficheroIndice, false))){
+            if (posicion != -1) {
+                dos.write(posicion);
+                dos.writeBytes("-" + dni);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
