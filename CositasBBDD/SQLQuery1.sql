aaff321fc9ce893df38b9ac21906fd5a068ec051
Santi <santima1904@gmail.com>
2022-02-06 20:41:03 +0100

    A

---
+++ CositasBBDD/SQLQuery1.sql
@@ -0,0 +1,32 @@
+USE MartinezAral
+GO
+--CREATE PROCEDURE SP_CrearFactura @idPedido int AS ;
+--INSERT INTO Factura(idPedido, Fecha) VALUES (@idPedido, CURRENT_TIMESTAMP)
+--GO
+
+--Mostrar una lista de productos que contiene la base de datos indicando el tipo.
+--Jardineria
+SELECT * FROM Producto AS P
+--Plantas
+SELECT P.codigo, P.descripcion, P.precioUnitario, P.unidades, TP.nombre FROM Producto AS P
+INNER JOIN ProductoPlanta AS PP ON P.codigo = PP.codigo
+INNER JOIN TipoPlanta AS TP ON PP.idTipoPlanta = TP.Id
+
+--Mostrar una lista de clientes.
+SELECT * FROM Cliente
+
+--Mostrar una lista de vendedores.
+SELECT * FROM UsuarioVendedor
+
+--Mostrar una lista de gestores.
+SELECT * FROM UsuarioGestor
+
+--Mostrar las facturas de un cliente concreto.
+SELECT * FROM Cliente 
+WHERE Id = @id
+
+--Generar un informe con el total de ventas de un mes indicando la fecha.
+--Este informe incluirá las ventas totales por cada tipo de producto y el total global.
+
+
+--Generar un informe de ventas de todo un año, con los mismos datos que el anterior.
