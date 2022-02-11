de02542bc20eb1f876b1969c2a169d8893298647
Santi <santima1904@gmail.com>
2022-02-11 19:28:08 +0100

    mi parte acabada

--- PracticaViveros/src/modelo/dataaccess/consultas/ConsultasInforme.java
+++ PracticaViveros/src/modelo/dataaccess/consultas/ConsultasInforme.java
@@ -88,18 +88,18 @@ public class ConsultasInforme {
     }
 
     /**
-     * <b>Cabecera: </b> public static List<Factura> getFacturasCliente(int id)</br>
-     * <b>Description: </b>Metodo para obtener un listado con todas los facturas de un cliente dado de la base de datos</br>
-     * <b>Precondiciones: </b>Ninguna</br>
-     * <b>Postcondiciones: </b>Lista rellena con las facturas</br>
+     * <b>Cabecera: </b> public static void getInformeMensual(int mes)</br>
+     * <b>Description: </b>Metodo para obtener un informe de las ventas de todos los productos en un mes dado de la base de datos</br>
+     * <b>Precondiciones: </b>mes positivo y diferente de 0</br>
+     * <b>Postcondiciones: </b>consulta obtenida</br>
      *
-     * @return
+     * @param mes
      */
     public static void getInformeMensual(int mes){
         Connection connection = ConexionBBDD.abrirConexion(new MiConexion());
         Statement statement = ConexionBBDD.crearStatement(connection);
         try(ResultSet rs = statement.executeQuery(INFORME_MENSUAL+mes)){
-            recogerInforme(rs);
+            recogerInforme(rs, true);
         } catch (SQLException e) {
             e.printStackTrace();
         }finally {
@@ -109,33 +109,78 @@ public class ConsultasInforme {
     }
 
     /**
-     * <b>Cabecera: </b>private static Factura crearFactura(ResultSet rs) throws SQLException</br>
-     * <b>Description: </b>Metodo para leer una factura de la base de datos</br>
+     * <b>Cabecera: </b> public static void getInformeAnual(int anho)</br>
+     * <b>Description: </b>Metodo para obtener un informe de las ventas de todos los productos en un anho dado de la base de datos</br>
+     * <b>Precondiciones: </b>anho positivo y diferente de 0</br>
+     * <b>Postcondiciones: </b>consulta obtenida</br>
+     *
+     * @param anho
+     */
+    public static void getInformeAnual(int anho){
+        Connection connection = ConexionBBDD.abrirConexion(new MiConexion());
+        Statement statement = ConexionBBDD.crearStatement(connection);
+        try(ResultSet rs = statement.executeQuery(INFORME_ANUAL+anho)){
+            recogerInforme(rs, false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            ConexionBBDD.cerrarStatement(statement);
+            ConexionBBDD.cerrarConexion(connection);
+        }
+    }
+
+    /**
+     * <b>Cabecera: </b>private static void recogerInforme(ResultSet rs, boolean esMes) throws SQLException</br>
+     * <b>Description: </b>Metodo para leer un informe de la base de datos</br>
      *
      * @param rs
+     * @param esMes
      * @return
      */
-    private static void recogerInforme(ResultSet rs) throws SQLException {
+    private static void recogerInforme(ResultSet rs, boolean esMes) throws SQLException {
         String consulta = "";
 
         ResultSetMetaData rsm = rs.getMetaData();
         for (int i = 1; i <= rsm.getColumnCount(); i++) {
             consulta += rs.getString(i) + " ";
         }
+        if (esMes){
+            mostrarInformeMensual(consulta);
+        }else{
+            mostrarInformeAnual(consulta);
+        }
         System.out.println(consulta);
     }
 
     /**
-     * <b>Cabecera: </b>private static Factura generarFactura(String consulta)</br>
-     * <b>Description: </b>Metodo para crear una factura</br>
+     * <b>Cabecera: </b>private static void mostrarInformeMensual(String consulta)</br>
+     * <b>Description: </b>Metodo para mostrar el informe por pantalla</br>
      *
      * @param consulta
-     * @return
      */
-    private static void mostrarInforme(String consulta){
+    private static void mostrarInformeMensual(String consulta){
         String [] atributos;
 
         atributos = consulta.split(" ");
-        System.out.println("Informe del mes "+ Month.of(Integer.parseInt(atributos[0])).getDisplayName(TextStyle.FULL_STANDALONE, new Locale(Locale.ENGLISH.getLanguage())));
+        System.out.println("Informe del mes de "+ Month.of(Integer.parseInt(atributos[0])).getDisplayName(TextStyle.FULL_STANDALONE, new Locale(Locale.ENGLISH.getLanguage())));
+        System.out.println("Informe de ventas de productos plantas: " + atributos[1]);
+        System.out.println("Informe de ventas de productos jardineria: " + atributos[2]);
+        System.out.println("Informe de ventas totales: " + atributos[3]);
+    }
+
+    /**
+     * <b>Cabecera: </b>private static void mostrarInformeAnual(String consulta)</br>
+     * <b>Description: </b>Metodo para mostrar el informe por pantalla</br>
+     *
+     * @param consulta
+     */
+    private static void mostrarInformeAnual(String consulta){
+        String [] atributos;
+
+        atributos = consulta.split(" ");
+        System.out.println("Informe del anual de " + atributos[0]);
+        System.out.println("Informe de ventas de productos plantas: " + atributos[1]);
+        System.out.println("Informe de ventas de productos jardineria: " + atributos[2]);
+        System.out.println("Informe de ventas totales: " + atributos[3]);
     }
 }
